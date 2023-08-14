86190cb84af50409ccb9dd8b9885552d7ab1cd78
xanderBolinger <52334326+xanderBolinger@users.noreply.github.com>
2023-08-14 08:59:17 -0700

    yay!!!

--- Assets/Animations/Animation Scripts/CharacterAnimator.cs
+++ Assets/Animations/Animation Scripts/CharacterAnimator.cs
@@ -5,6 +5,47 @@ using UnityEngine;
 public class CharacterAnimator : MonoBehaviour
 {
 
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void SetIdle() {
+       if(animator == null) { return; }
+
+       ClearAnimation();
+    }
+
+    public void SetWalk() {
+        if (animator == null) { return; }
+
+        ClearAnimation();
+        animator.SetBool("IsWalking", true);
+    }
+
+    public void SetSwing() {
+        if (animator == null) { return; }
+
+        ClearAnimation();
+        animator.SetBool("IsSwinging", true);
+    }
+
+    public void SetStab() {
+        if (animator == null) { return; }
+
+        ClearAnimation();
+        animator.SetBool("IsStabbing", true);
+    }
+
+    private void ClearAnimation() {
+        if (animator == null) { return; }
+
+          animator.SetBool("IsWalking", false);
+          animator.SetBool("IsSwinging", false);
+          animator.SetBool("IsStabbing", false);
+    }
 
     public void DeactivateAll() { 
         GetComponent<IdleAnimator>()?.Deactivate();
