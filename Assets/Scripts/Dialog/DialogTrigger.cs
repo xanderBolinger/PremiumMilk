3456bb5187d6dda3c1fcd9fa6525a8b215d81515
20Julie20 <112004552+20Julie20@users.noreply.github.com>
2023-08-16 20:03:45 -0600

    strangling my code

--- Assets/Scripts/Dialog/DialogTrigger.cs
+++ Assets/Scripts/Dialog/DialogTrigger.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DialogTrigger : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class DialogTrigger : MonoBehaviour {
+
+    [SerializeField] public GameObject PopUp;
+    [SerializeField] public TextAsset inkJSON;
+
+    private bool inRange;
+
+    private void Awake() {
+        inRange = false;
+        PopUp.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        if (inRange) {
+            PopUp.SetActive(true);
+            /*if (InputManager.GetInstance().GetInteractPressed()) {
+                Debug.log(inkJSON.text);
+            }*/
+        } else {
+            PopUp.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider col) {
+        if (col.gameObject.tag == "Player") {
+            inRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider col) {
+        if (col.gameObject.tag == "Player") {
+            inRange = false;
+        }
     }
 }
