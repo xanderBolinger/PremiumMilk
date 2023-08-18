537bc525c4518746a40dad18bb2a26e16102392a
xanderBolinger <52334326+xanderBolinger@users.noreply.github.com>
2023-08-18 15:41:04 -0700

    slurp

--- Assets/Scripts/Dialog/DialogTrigger.cs
+++ Assets/Scripts/Dialog/DialogTrigger.cs
@@ -5,6 +5,7 @@ using Ink.Runtime;
 using TMPro;
 using System;
 using UnityEngine.UI;
+using Unity.VisualScripting;
 
 public class DialogTrigger : MonoBehaviour {
 
@@ -13,9 +14,8 @@ public class DialogTrigger : MonoBehaviour {
 
     [SerializeField] private GameObject PopUp;
     [SerializeField] private TextAsset inkJSON;
-
-    [SerializeField] private VerticalLayoutGroup choiceButtonContainer;
     [SerializeField] private Button choiceButtonPrefab;
+    [SerializeField] private GameObject buttonContainer;
 
     private bool inRange;
     private Story story;
@@ -24,7 +24,6 @@ public class DialogTrigger : MonoBehaviour {
         inRange = false;
         PopUp.SetActive(false);
         story = new Story(inkJSON.text);
-        Debug.Log(story.Continue());
     }
 
     private void Update() {
@@ -48,17 +47,44 @@ public class DialogTrigger : MonoBehaviour {
         }
     }
 
+    Button CreateChoiceButton(string text) {
+        // creates the button from a prefab
+        var choiceButton = Instantiate(choiceButtonPrefab);
+        choiceButton.transform.parent = buttonContainer.transform;
+
+        // sets text on the button
+        var buttonText = choiceButton.GetComponentInChildren<TextMeshProUGUI>();
+        buttonText.text = text;
+
+        return choiceButton;
+    }
+
+    void OnClickChoiceButton(Choice choice) {
+        story.ChooseChoiceIndex(choice.index); // tells ink which choice was selected
+        RefreshChoiceView(); // removes choices from the screen
+        UpdateText();
+
+    }
+
+    private void RefreshChoiceView() {
+        if (buttonContainer != null) {
+            foreach(Transform button in buttonContainer.transform){
+                Destroy(button.gameObject);
+            }
+        }
+    }
+
     private void UpdateText() {
         while(story.canContinue) {
             dialogPopupText.text = story.ContinueMaximally();
 
             if (story.currentChoices.Count > 0) {
-                string choices = "";
                 for (int i = 0; i < story.currentChoices.Count; ++i) {
-                    Choice choice = story.currentChoices[i];
-                    choices += choice.text;
+                    var choice = story.currentChoices[i];
+                    var button = CreateChoiceButton(choice.text); // creates a choice button
+
+                    button.onClick.AddListener(() => OnClickChoiceButton(choice));
                 }
-                dialogPopupText.text = choices;
             }
         }
     }
