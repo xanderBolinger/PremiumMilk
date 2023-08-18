5edcbccc4d82520a93dcbc063c1aec7755739e1a
xanderBolinger <52334326+xanderBolinger@users.noreply.github.com>
2023-08-18 15:01:00 -0700

    l

--- Assets/Scripts/Dialog/DialogTrigger.cs
+++ Assets/Scripts/Dialog/DialogTrigger.cs
@@ -1,28 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Ink.Runtime;
+using TMPro;
+using System;
+using UnityEngine.UI;
 
 public class DialogTrigger : MonoBehaviour {
+
+
+    public TextMeshProUGUI dialogPopupText;
 
     [SerializeField] private GameObject PopUp;
     [SerializeField] private TextAsset inkJSON;
 
+    [SerializeField] private VerticalLayoutGroup choiceButtonContainer;
+    [SerializeField] private Button choiceButtonPrefab;
+
     private bool inRange;
+    private Story story;
 
     private void Awake() {
         inRange = false;
         PopUp.SetActive(false);
+        story = new Story(inkJSON.text);
+        Debug.Log(story.Continue());
     }
 
     private void Update() {
        if (inRange) {
            PopUp.SetActive(true);
-           /*if (InputManager.GetInstance().GetInteractPressed()) {
-               Debug.log(inkJSON.text);
-           }*/
+           UpdateText();
        } else {
-           PopUp.SetActive(false);
-      }
+            PopUp.SetActive(false);
+        }
     }
 
     private void OnTriggerEnter(Collider col) {
@@ -34,6 +45,21 @@ public class DialogTrigger : MonoBehaviour {
     private void OnTriggerExit(Collider col) {
         if (col.gameObject.tag == "Player") {
             inRange = false;
+        }
+    }
+
+    private void UpdateText() {
+        while(story.canContinue) {
+            dialogPopupText.text = story.ContinueMaximally();
+
+            if (story.currentChoices.Count > 0) {
+                string choices = "";
+                for (int i = 0; i < story.currentChoices.Count; ++i) {
+                    Choice choice = story.currentChoices[i];
+                    choices += choice.text;
+                }
+                dialogPopupText.text = choices;
+            }
         }
     }
 }
