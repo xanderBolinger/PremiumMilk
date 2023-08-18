a9991a05a8826a313fbbf4c86e98974190c90158
xander@cuttingedgemachining.onmicrosoft.com <xanderleebolinger@gmail.com>
2023-08-18 14:55:08 -0600

    c2

---
+++ Assets/Editor/ClothingManagerEditor.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ClothingManager))]
+public class ClothingManagerEditorGui : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        ClothingManager cm = (ClothingManager)target;
+
+        if (GUILayout.Button("Equip Item"))
+        {
+            cm.EquipItem(cm.testItemName, cm.testItemType, cm.testSlotType);
+        }
+
+
+
+
+    }
+}
--- Assets/Scripts/CharacterScripts/ClothingManager.cs
+++ Assets/Scripts/CharacterScripts/ClothingManager.cs
@@ -1,24 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class ClothingManager : MonoBehaviour
 {
 
+    public SlotType testSlotType;
+    public ItemType testItemType;
+    public string testItemName = "MailHalberk";
 
-    public void EquipChestArmor() { 
+    public enum SlotType { 
     
+        ChestArmor
     }
 
+    public enum ItemType { 
+        Armor
+    }
+
+    public void EquipItem(string name, ItemType itemType, SlotType slotType) {
+
+       var prefab = FindPrefab(name, itemType.ToString());
+
+        var slots = FindSlots(slotType.ToString());
+
+        foreach (var slot in slots) {
+            foreach (Transform child in slot.transform)
+            {
+                Destroy(child.gameObject);
+            }
+            var instance = Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+            instance.transform.parent = slot.transform;
+            instance.transform.localPosition = Vector3.zero;
+            instance.transform.rotation = Quaternion.identity;
+            instance.transform.localRotation = Quaternion.identity;
+            instance.transform.localScale = Vector3.one;
+            
+        }
+    }
+
+
+    public GameObject FindPrefab(string name, string itemType)
+    {
+        GameObject asset = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/"+ itemType + "/"+name+".prefab");
+
+        return asset;
+    }
 
     public List<GameObject> FindSlots(string name) {
 
         var list = new List<GameObject>();
 
         foreach (Transform childTransform in transform) {
-            if (childTransform.gameObject.name == name)
-                list.Add(childTransform.gameObject);
-
+            foreach (Transform grandchildTransform in childTransform) {
+                if (grandchildTransform.gameObject.name == name)
+                    list.Add(grandchildTransform.gameObject);
+            }
         }
 
         return list; 
