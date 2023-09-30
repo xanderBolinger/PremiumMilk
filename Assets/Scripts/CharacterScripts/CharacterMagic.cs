5aedf4b6519a92b6195aecc5442fd488b3e58634
xander@cuttingedgemachining.onmicrosoft.com <xanderleebolinger@gmail.com>
2023-09-30 12:33:05 -0600

    fixed collider issue

--- Assets/Scripts/CharacterScripts/CharacterMagic.cs
+++ Assets/Scripts/CharacterScripts/CharacterMagic.cs
@@ -18,17 +18,19 @@ public class CharacterMagic : NetworkBehaviour
 
         castedSpell = true;
 
-        // This method gets called on server
-        CmdCastSpell();
+        // This method gets called on server, target name has to be passed because it is not set on the server 
+        // Could use a sync var but they are kinda annoying
+        CmdCastSpell(targetName);
     }
 
+    // Use this method to set target name from your player input code once you have clicked on a selected target
     public void GetTargetName(GameObject selectedTarget) {
         targetName = selectedTarget.GetComponent<CharacterNetwork>().characterName;
     }
 
     // Called on server
     [Command]
-    private void CmdCastSpell() {
+    private void CmdCastSpell(string targetName) {
         MagicManager.magicManager.CastSpell(castSpellType, targetName, castPos);
     }
 
