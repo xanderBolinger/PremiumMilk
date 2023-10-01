1f16098a9b7cfed1a79276c5430e21ad3e33e50e
xanderBolinger <52334326+xanderBolinger@users.noreply.github.com>
2023-09-30 22:56:43 -0600

    l

--- Assets/Scripts/SettingTilesAlgorithm/Algorithm.cs
+++ Assets/Scripts/SettingTilesAlgorithm/Algorithm.cs
@@ -56,7 +56,6 @@ public class Algorithm : MonoBehaviour {
                     t.RemoveAt(i);
                     GetChildren(t[i].gameObject, t);
                 }
-
             }
 
             if (count == 4) break;
