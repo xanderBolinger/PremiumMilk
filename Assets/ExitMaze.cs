c246e02ad909dbf7ccd8d7edc584153f3d27bf5d
xanderBolinger <52334326+xanderBolinger@users.noreply.github.com>
2023-10-01 21:41:12 -0600

    c

--- Assets/ExitMaze.cs
+++ Assets/ExitMaze.cs
@@ -7,6 +7,7 @@ public class ExitMaze : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         var obj = collision.gameObject;
+
         if (obj.tag != "Character" && obj.GetComponent<CharacterController>().player) {
 
             SceneManager.LoadScene("End");
